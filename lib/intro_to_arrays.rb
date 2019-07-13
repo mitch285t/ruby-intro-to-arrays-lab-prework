dda14858c2e881ea7314da0948f8a6d7eb4e8b62
mitchell williams <mscottwilliams1994@gmail.com>
2019-07-13 19:24:22 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -10,3 +10,5 @@ taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods
 def first_element(taylor_swift)
   my_first_element = taylor_swift[0]
 end
+def third_element(taylor_swift)
+  my_third_element = taylor_swift[3]
