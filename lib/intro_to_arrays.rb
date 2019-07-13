9076b01af620f39700d318607b51eb00d36ab20a
mitchell williams <mscottwilliams1994@gmail.com>
2019-07-13 19:23:38 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -7,6 +7,6 @@ end
 
 taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
 
-def first_element(array)
+def first_element(taylor_swift)
   my_first_element = taylor_swift[0]
 end
