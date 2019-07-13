e76bf4c9a35a2a2d8ddca8a42cb84ca1ca8584a8
mitchell williams <mscottwilliams1994@gmail.com>
2019-07-13 19:24:50 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -11,5 +11,5 @@ def first_element(taylor_swift)
   my_first_element = taylor_swift[0]
 end
 def third_element(taylor_swift)
-  my_third_element = taylor_swift[3]
+  my_third_element = taylor_swift[2]
 end
