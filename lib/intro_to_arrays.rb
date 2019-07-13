c74c04d1ea86b7458fc5a5f8f6ac8d556fed0e34
mitchell williams <mscottwilliams1994@gmail.com>
2019-07-13 19:20:25 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,12 @@
+def instantiate_new_array
+  my_new_array = []
+end
+def array_with_two_elements
+  my_two_array = ["one", "two"]
+end
+def index
+taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+end
+def first_element(array)
+  my_first_element = taylor_swift[0]
+end
